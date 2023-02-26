71e0d932fd51de1bfefa867ec3ff8e5226630f3a
Liz Burton <liz.burton147@gmail.com>
2023-02-26 15:05:49 -0500

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "birds#index"
   get '/birds/random', to: 'birds#random'
   get '/birds/appledore', to: 'birds#appledore'
   get '/birds/appledore/random', to: 'birds#appledore_random'
-  resources :birds
+
 end
