f2d99d3adc80944d540ae2422173ed8f6a769b0f
Liz Burton <liz.burton147@gmail.com>
2023-02-26 16:08:52 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "birds#index"
+  get '/birds', to:'birds#index'
   get '/birds/random', to: 'birds#random'
   get '/birds/appledore', to: 'birds#appledore'
   get '/birds/appledore/random', to: 'birds#appledore_random'
+  root "birds#index"
 
 end
